7dd708ea4507931ce117043e5fe861ed415d1fee
hizbashidiq <hizbulloh.ash@gmail.com>
2021-11-21 09:15:48 +0700

    create DSA

--- src/services/DSA/key.py
+++ src/services/DSA/key.py
@@ -1,7 +1,5 @@
 from type_aliases import key
 
-
-# TODO : implement key generator for DSA algorithm
 def generate_key(p: int, q: int, x: int) -> (key, key):
     '''Generate DSA public/private key using given prime
      numbers p and q and private key x.
@@ -9,4 +7,9 @@ def generate_key(p: int, q: int, x: int) -> (key, key):
     It's assumed that q is prime and a factor of (p - 1) while
     x is a random integer less than q.'''
 
-    pass
+    g = pow(100,int((p-1)/q), p)
+    y = pow(g,x,p)
+
+    pub_key: key = {'p': p, 'q': q, 'g': g, 'y': y}
+    pri_key: key = {'x': x}
+    return pub_key, pri_key
--- src/services/DSA/main.py
+++ src/services/DSA/main.py
@@ -1,19 +1,27 @@
 from type_aliases import dsign
+from random import randint
 
-
-# TODO : implement sign function using DSA algorithm.
 def sign(_hash: int, p: int, q: int, g: int, x: int) -> dsign:
-    '''Generate a signature from given hash
-    and private key (p, q, g, x) using DSA
-    algorithm.'''
+    '''Generate a signature from given hash,
+    and private key (p, q, g) using DSA algorithm.'''
 
-    pass
+    k = randint(1, q-1)
 
+    r = pow(pow(g,k,p),1,q)
+    s = pow((pow(k, -1, q)*(_hash + (x*r))),1,q)
+    
+    return r,s
 
-# TODO : implement verify function using DSA algorithm
 def verify(_hash: int, sign: dsign, p: int, q: int, g: int, y: int) -> bool:
     '''Verify wether given sign (r, s) is 
     valid or not with given public key
     (p, q, g, y) using DSA algorithm.'''
+    
+    s_inv = pow(sign[1], -1, q)
+    w = pow(s_inv,1,q)
 
-    pass
+    u1 = (_hash*w) % q
+    u2 = (sign[0]*w) % q
+    v = ((g**u1*y**u2) % p) % q
+
+    return v == sign[0]
